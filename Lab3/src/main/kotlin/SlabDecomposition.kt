1c9a4c46c44c377779be6ec3be2ea3a2557fb916
Unknown <justataya@gmail.com>
2020-02-19 22:29:43 +0200

    Progress commit

--- Lab3/src/main/kotlin/SlabDecomposition.kt
+++ Lab3/src/main/kotlin/SlabDecomposition.kt
@@ -12,7 +12,6 @@ class SlabDecomposition(private val graph: Graph, private val point: Pair<Double
         val stripes = mutableListOf<Stripe>()
         val edges: TreeSet<Pair<GraphNode, GraphNode>> = TreeSet()
 
-
         for (i in nodes) {
             println("${i.x} ${i.y}")
             for (j in i.getEdges()) {
@@ -24,5 +23,30 @@ class SlabDecomposition(private val graph: Graph, private val point: Pair<Double
             }
             stripes += Stripe(i, edges.clone() as TreeSet<Pair<GraphNode, GraphNode>>)
         }
+
+        val stripe = findStipe(point, stripes)
+    }
+
+    //binary search in stripes
+    private fun findStipe(point: Pair<Double, Double>, stripes: List<Stripe>): Stripe {
+        var left = 0
+        var right = stripes.size - 1
+        var mid = 0
+
+        while (left < right) {
+            mid = left + (left + right) / 2
+            if (point.second < stripes[mid].node.y) {
+                right = mid
+            } else {
+                left = mid
+            }
+        }
+
+        return stripes[mid]
+    }
+
+
+    private fun findEdge(point: Pair<Double, Double>, stripe: Stripe){
+        
     }
 }
