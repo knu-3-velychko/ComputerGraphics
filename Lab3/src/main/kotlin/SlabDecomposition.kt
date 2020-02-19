a612e1884f82752f72566d9c39b4d44eef26ddcf
Tai <justataya@gmail.com>
2020-02-20 01:04:17 +0300

    Progress commit

--- Lab3/src/main/kotlin/SlabDecomposition.kt
+++ Lab3/src/main/kotlin/SlabDecomposition.kt
@@ -24,16 +24,17 @@ class SlabDecomposition(private val graph: Graph, private val point: Pair<Double
             stripes += Stripe(i, edges.clone() as TreeSet<Pair<GraphNode, GraphNode>>)
         }
 
-        val stripe = findStipe(point, stripes)
+        val stripe = findStripe(point, stripes)
+        val edge = findEdge(point, stripe.edges.toList())
     }
 
     //binary search in stripes
-    private fun findStipe(point: Pair<Double, Double>, stripes: List<Stripe>): Stripe {
+    private fun findStripe(point: Pair<Double, Double>, stripes: List<Stripe>): Stripe {
         var left = 0
         var right = stripes.size - 1
         var mid = 0
 
-        while (left < right) {
+        while (right - left > 1) {
             mid = left + (left + right) / 2
             if (point.second < stripes[mid].node.y) {
                 right = mid
@@ -46,7 +47,29 @@ class SlabDecomposition(private val graph: Graph, private val point: Pair<Double
     }
 
 
-    private fun findEdge(point: Pair<Double, Double>, stripe: Stripe){
-        
+    private fun findEdge(
+        point: Pair<Double, Double>,
+        edges: List<Pair<GraphNode, GraphNode>>
+    ): Pair<GraphNode, GraphNode> {
+        var left = 0
+        var right = edges.size - 1
+        var mid = 0
+
+        while (right - left > 1) {
+            if (rotate(edges[mid].first, edges[mid].second, point) < 0)
+                right = mid
+            else
+                left = mid
+        }
+
+        return edges[mid]
     }
+
+    private fun createPolygon(edge: Pair<GraphNode, GraphNode>): List<GraphNode> {
+        //TODO
+        return listOf()
+    }
+
+    private fun rotate(a: GraphNode, b: GraphNode, c: Pair<Double, Double>) =
+        (b.x - a.x) * (c.second - b.y) - (b.y - a.y) * (c.first - b.x)
 }
